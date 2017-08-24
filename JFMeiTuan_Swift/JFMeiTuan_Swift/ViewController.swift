493ba7ffd8668dd787a69172c630fbf194eee2ce
linjf <linjf@fenqi.im>
2017-08-24 12:52:21 +0800

    第一次提交

---
+++ JFMeiTuan_Swift/JFMeiTuan_Swift/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  JFMeiTuan_Swift
+//
+//  Created by linjianfang on 2017/8/24.
+//  Copyright © 2017年 linjianfang. All rights reserved.
+// 第一次提交
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
