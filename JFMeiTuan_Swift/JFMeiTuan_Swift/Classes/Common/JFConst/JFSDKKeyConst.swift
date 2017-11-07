bc23671fe5051819e6b75ba2ffb97857c42d6e8f
linjf <linjf@fenqi.im>
2017-11-07 15:48:27 +0800

    添加常量的类

---
+++ JFMeiTuan_Swift/JFMeiTuan_Swift/Classes/Common/JFConst/JFSDKKeyConst.swift
@@ -0,0 +1,13 @@
+//
+//  JFSDKKeyConst.swift
+//  JFMeiTuan_Swift
+//
+//  Created by linjianfang on 2017/11/7.
+//  Copyright © 2017年 linjianfang. All rights reserved.
+//
+
+import UIKit
+
+class JFSDKKeyConst: NSObject {
+
+}
