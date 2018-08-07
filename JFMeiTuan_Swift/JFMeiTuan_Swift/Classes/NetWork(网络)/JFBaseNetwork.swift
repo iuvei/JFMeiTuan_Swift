e82bbf5d6139a16f0dac3e32d34901f2d980bab9
linjf@2345.com <linjf@2345.com>
2018-08-07 16:30:07 +0800

    添加闭包请求

--- JFMeiTuan_Swift/JFMeiTuan_Swift/Classes/NetWork(网络)/JFBaseNetwork.swift
+++ JFMeiTuan_Swift/JFMeiTuan_Swift/Classes/NetWork(网络)/JFBaseNetwork.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2018年 linjianfang. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Alamofire
+
+enum MethodType {
+    case get
+    case post
+}
+
+//定义一个block闭包
+typealias success = () -> Any
+typealias failure = () -> Error
+
+
+
+class JFBaseNetwork: NSObject {
+    var value = ""
+    static let shareInstance = JFBaseNetwork()
+    private override init(){}
+    
+    
+    let successBack =  success.self
+    let failureBack =  failure.self
+    
+}
+
+
+
+
