81c134ab9e95c616aca58fdb0d2e69f00b1539be
linjf@2345.com <linjf@2345.com>
2018-08-09 18:19:45 +0800

    添加我的附近

--- JFMeiTuan_Swift/JFMeiTuan_Swift/Classes/Near/JFNearViewController.swift
+++ JFMeiTuan_Swift/JFMeiTuan_Swift/Classes/Near/JFNearViewController.swift
@@ -2,22 +2,36 @@
 //  JFNearViewController.swift
 //  JFMeiTuan_Swift
 //
-//  Created by linjianfang on 2017/8/29.
-//  Copyright © 2017年 linjianfang. All rights reserved.
+//  Created by linjf on 2018/8/9.
+//  Copyright © 2018年 linjianfang. All rights reserved.
 //
 
 import UIKit
 
-class JFNearViewController: JFBaseViewController {
+class JFNearViewController: JFBaseViewController,UITableViewDataSource,UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.title = "附近"
         // Do any additional setup after loading the view.
-        self.title = "附近"
-        self.view.backgroundColor = UIColor.orange
     }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 30
+        
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let cell:JFNormalOrderCell  = JFNormalOrderCell.cellWithTableView(tableView)
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView .deselectRow(at: indexPath, animated: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
