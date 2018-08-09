52ca16717eee1a09c471820525056f76f0fe2036
linjf@2345.com <linjf@2345.com>
2018-08-09 11:45:23 +0800

    添加一个cell

--- JFMeiTuan_Swift/JFMeiTuan_Swift/Classes/Walk/Controller/JFWalkChildViewController.swift
+++ JFMeiTuan_Swift/JFMeiTuan_Swift/Classes/Walk/Controller/JFWalkChildViewController.swift
@@ -23,13 +23,32 @@ class JFWalkChildViewController: JFBaseViewController,UITableViewDataSource,UITa
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         
-        let cell:JFWeakNormalCell  = JFWeakNormalCell.cellWithTableView(tableView)
-        return cell
         
+        let type  = indexPath.row % 2
+        if type == 0 {
+            let cell:JFWeakNormalCell  = JFWeakNormalCell.cellWithTableView(tableView)
+            return cell
+
+        }else{
+            let cell:JFWalkVideoCell  = JFWalkVideoCell.cellWithTableView(tableView)
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let type  = indexPath.row % 2
+        if type == 0 {
+            return 120
+            
+        }else{
+            return 200
+        }
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView .deselectRow(at: indexPath, animated: true)
+        
+       
         
         let VC:JFWebViewController =  JFWebViewController.init()
         VC.hidesBottomBarWhenPushed = true
