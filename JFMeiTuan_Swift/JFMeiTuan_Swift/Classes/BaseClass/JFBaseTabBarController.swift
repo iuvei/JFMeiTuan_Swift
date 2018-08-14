a3e116dc1412497e68d2a1390f1788c6b425efb3
linjf@2345.com <linjf@2345.com>
2018-08-14 14:40:44 +0800

    优化

--- JFMeiTuan_Swift/JFMeiTuan_Swift/Classes/BaseClass/JFBaseTabBarController.swift
+++ JFMeiTuan_Swift/JFMeiTuan_Swift/Classes/BaseClass/JFBaseTabBarController.swift
@@ -14,61 +14,60 @@ class JFBaseTabBarController: UITabBarController {
         super.viewDidLoad()
 
         self.tabBar.isTranslucent = false//tabbar不透明
-
-        // Do any additional setup after loading the view.
-        
         setChildrenControllers()
-//         initControllers()
+
     }
     
+    /*
      func initControllers(){
-        let JFBaseHomeNav = JFBaseNacigationController.init(rootViewController: JFHomeViewController())
-        let JFBaseNearNav = JFBaseNacigationController.init(rootViewController: JFNearViewController())
-        let JFBaseWalkNav = JFBaseNacigationController.init(rootViewController: JFwalkViewController())
-        let JFBaseOrderNav = JFBaseNacigationController.init(rootViewController: JFOrderViewController())
-//        let JFBaseMeNav = JFBaseNacigationController.init(rootViewController: JFMeViewController())
-        let JFBaseMeNav = JFBaseNacigationController.init(rootViewController: JFMineViewController())
-
-        self.viewControllers = [JFBaseHomeNav,JFBaseNearNav,JFBaseWalkNav,JFBaseOrderNav,JFBaseMeNav]
-//        self.selectedViewController = JFBaseMeNav
-        self.tabBar.isTranslucent = false//tabbar不透明
-        
-        let tabbarItem0 = self.tabBar.items?[0]
-        tabbarItem0?.image = UIImage.init(named: "pfb_tabbar_homepage")
-        tabbarItem0?.selectedImage = UIImage.init(named: "pfb_tabbar_homepage_selected")
-        tabbarItem0?.title = "首页";
-        tabbarItem0?.titlePositionAdjustment = UIOffsetMake(0,-4)
-        
-        let tabbarItem1 = self.tabBar.items?[1]
-        tabbarItem1?.image = UIImage.init(named: "pfb_tabbar_merchant")
-        tabbarItem1?.selectedImage = UIImage.init(named: "pfb_tabbar_merchant_selected")
-        tabbarItem1?.title = "附近";
-        tabbarItem1?.titlePositionAdjustment = UIOffsetMake(0,-4)
-        
-        let tabbarItem2 = self.tabBar.items?[2]
-        tabbarItem2?.image = UIImage.init(named: "pfb_tabbar_discover")
-        tabbarItem2?.selectedImage = UIImage.init(named: "pfb_tabbar_discover_selected")
-        tabbarItem2?.title = "发现";
-        tabbarItem2?.titlePositionAdjustment = UIOffsetMake(0,-4)
-        
-        let tabbarItem3 = self.tabBar.items?[3]
-        tabbarItem3?.image = UIImage.init(named: "pfb_tabbar_order")
-        tabbarItem3?.selectedImage = UIImage.init(named: "pfb_tabbar_order_selected")
-        tabbarItem3?.title = "订单";
-        tabbarItem3?.titlePositionAdjustment = UIOffsetMake(0,-4)
-        
-        let tabbarItem4 = self.tabBar.items?[4]
-        tabbarItem4?.image = UIImage.init(named: "pfb_tabbar_mine")
-        tabbarItem4?.selectedImage = UIImage.init(named: "pfb_tabbar_order_selected")
-        tabbarItem4?.title = "我的";
-        tabbarItem4?.titlePositionAdjustment = UIOffsetMake(0,-4)
-        
-        
-        //选中个非选中的字体颜色
-        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.gray], for:.normal)
-        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: JFStyle.navigarionBarColor()], for: .selected)
-        
-    }
+     let JFBaseHomeNav = JFBaseNacigationController.init(rootViewController: JFHomeViewController())
+     let JFBaseNearNav = JFBaseNacigationController.init(rootViewController: JFNearViewController())
+     let JFBaseWalkNav = JFBaseNacigationController.init(rootViewController: JFwalkViewController())
+     let JFBaseOrderNav = JFBaseNacigationController.init(rootViewController: JFOrderViewController())
+     let JFBaseMeNav = JFBaseNacigationController.init(rootViewController: JFMineViewController())
+     
+     self.viewControllers = [JFBaseHomeNav,JFBaseNearNav,JFBaseWalkNav,JFBaseOrderNav,JFBaseMeNav]
+     self.tabBar.isTranslucent = false//tabbar不透明
+     
+     let tabbarItem0 = self.tabBar.items?[0]
+     tabbarItem0?.image = UIImage.init(named: "pfb_tabbar_homepage")
+     tabbarItem0?.selectedImage = UIImage.init(named: "pfb_tabbar_homepage_selected")
+     tabbarItem0?.title = "首页";
+     tabbarItem0?.titlePositionAdjustment = UIOffsetMake(0,-4)
+     
+     let tabbarItem1 = self.tabBar.items?[1]
+     tabbarItem1?.image = UIImage.init(named: "pfb_tabbar_merchant")
+     tabbarItem1?.selectedImage = UIImage.init(named: "pfb_tabbar_merchant_selected")
+     tabbarItem1?.title = "附近";
+     tabbarItem1?.titlePositionAdjustment = UIOffsetMake(0,-4)
+     
+     let tabbarItem2 = self.tabBar.items?[2]
+     tabbarItem2?.image = UIImage.init(named: "pfb_tabbar_discover")
+     tabbarItem2?.selectedImage = UIImage.init(named: "pfb_tabbar_discover_selected")
+     tabbarItem2?.title = "发现";
+     tabbarItem2?.titlePositionAdjustment = UIOffsetMake(0,-4)
+     
+     let tabbarItem3 = self.tabBar.items?[3]
+     tabbarItem3?.image = UIImage.init(named: "pfb_tabbar_order")
+     tabbarItem3?.selectedImage = UIImage.init(named: "pfb_tabbar_order_selected")
+     tabbarItem3?.title = "订单";
+     tabbarItem3?.titlePositionAdjustment = UIOffsetMake(0,-4)
+     
+     let tabbarItem4 = self.tabBar.items?[4]
+     tabbarItem4?.image = UIImage.init(named: "pfb_tabbar_mine")
+     tabbarItem4?.selectedImage = UIImage.init(named: "pfb_tabbar_order_selected")
+     tabbarItem4?.title = "我的";
+     tabbarItem4?.titlePositionAdjustment = UIOffsetMake(0,-4)
+     
+     
+     //选中个非选中的字体颜色
+     UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.gray], for:.normal)
+     UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: JFStyle.navigarionBarColor()], for: .selected)
+     
+     }
+     
+     */
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -104,7 +103,6 @@ extension JFBaseTabBarController{
             data = NSData(contentsOfFile: filePath!)
         }
         
-        //
         guard let arr = try? JSONSerialization.jsonObject(with: data! as Data, options: []) as?  [[String : AnyObject]]
             else {
                 return
@@ -139,16 +137,12 @@ extension JFBaseTabBarController{
         //设置图片
         rootVc.tabBarItem.image = UIImage(named:NorImgName)
         rootVc.tabBarItem.selectedImage = UIImage(named:SellectImgName)?.withRenderingMode(.alwaysOriginal)
-        
-        //设置tabbar标题字体
-        ///: 'NSForegroundColorAttributeName' has been renamed to 'NSAttributedStringKey.foregroundColor'
-        ///Cannot convert value of type 'NSAttributedStringKey' to expected argument type '[NSAttributedStringKey : Any]?'
+
         
         rootVc.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.gray], for: .selected)
         rootVc.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName: JFStyle.navigarionBarColor()], for: .selected)
 
-        /// 'NSFontAttributeName' has been renamed to 'NSAttributedStringKey.font'
-        rootVc.tabBarItem.setTitleTextAttributes([NSFontAttributeName:UIFont.systemFont(ofSize: 11.0)], for: .normal)
+        rootVc.tabBarItem.setTitleTextAttributes([NSFontAttributeName:UIFont.systemFont(ofSize: 9.0)], for: .normal)
         
         //实例化导航控制器
         let nav = JFBaseNacigationController(rootViewController: rootVc)
@@ -158,7 +152,6 @@ extension JFBaseTabBarController{
 }
 
 //获取命名空间
-
 extension Bundle{
     // 计算型属性类似于函数，没有参数，有返回值
     var namespace: String{
