11ff521088da22a0e46147c019695007de241958
linjf@2345.com <linjf@2345.com>
2018-08-14 14:30:19 +0800

    设置通用的tabbar

--- JFMeiTuan_Swift/JFMeiTuan_Swift/Classes/BaseClass/JFBaseTabBarController.swift
+++ JFMeiTuan_Swift/JFMeiTuan_Swift/Classes/BaseClass/JFBaseTabBarController.swift
@@ -13,13 +13,15 @@ class JFBaseTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.tabBar.isTranslucent = false//tabbar不透明
+
         // Do any additional setup after loading the view.
-         initControllers()
+        
+        setChildrenControllers()
+//         initControllers()
     }
     
      func initControllers(){
-
-    
         let JFBaseHomeNav = JFBaseNacigationController.init(rootViewController: JFHomeViewController())
         let JFBaseNearNav = JFBaseNacigationController.init(rootViewController: JFNearViewController())
         let JFBaseWalkNav = JFBaseNacigationController.init(rootViewController: JFwalkViewController())
@@ -68,7 +70,6 @@ class JFBaseTabBarController: UITabBarController {
         
     }
 
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -86,3 +87,84 @@ class JFBaseTabBarController: UITabBarController {
     */
 
 }
+
+extension JFBaseTabBarController{
+    
+    fileprivate func setChildrenControllers() {
+        //沙盒加载json
+        let docStr = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let jsonPath = (docStr as NSString).appendingPathComponent("main.json")
+        var data = NSData(contentsOfFile: jsonPath)
+        
+        //如果沙盒中没有main.json,从本地加载
+        if data == nil{
+            
+            //获取值为nil的问题: 项目-->Build Phases-->copy Bundle Resource中没有加入此文件
+            let filePath = Bundle.main.path(forResource: "main", ofType: "json")
+            data = NSData(contentsOfFile: filePath!)
+        }
+        
+        //
+        guard let arr = try? JSONSerialization.jsonObject(with: data! as Data, options: []) as?  [[String : AnyObject]]
+            else {
+                return
+        }
+        
+        var arrM = [UIViewController]()
+        for dict in arr! {
+            arrM.append(childrenController(dict: dict as [String : AnyObject]))
+        }
+        viewControllers = arrM
+    }
+    
+    //使用字典加载控制器
+    fileprivate func childrenController(dict: [String : AnyObject]) -> UIViewController {
+        
+        //取得内容
+        guard let RootVcName = dict["RootVcName"] as? String,
+            let TabbarTitle = dict["TabbarTitle"] as? String,
+            let NorImgName = dict["NorImgName"] as? String,
+            let SellectImgName = dict["SellectImgName"] as? String,
+            let RootVc = NSClassFromString(Bundle.main.namespace + "." + RootVcName) as? JFBaseViewController.Type
+            else {
+                return UIViewController()
+        }
+        
+        //创建控制器视图
+        let rootVc = RootVc.init()
+        rootVc.title = TabbarTitle
+        rootVc.tabBarController?.tabBarItem.title = TabbarTitle
+        rootVc.view.backgroundColor = UIColor.white
+        
+        //设置图片
+        rootVc.tabBarItem.image = UIImage(named:NorImgName)
+        rootVc.tabBarItem.selectedImage = UIImage(named:SellectImgName)?.withRenderingMode(.alwaysOriginal)
+        
+        //设置tabbar标题字体
+        ///: 'NSForegroundColorAttributeName' has been renamed to 'NSAttributedStringKey.foregroundColor'
+        ///Cannot convert value of type 'NSAttributedStringKey' to expected argument type '[NSAttributedStringKey : Any]?'
+        
+        rootVc.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.gray], for: .selected)
+        rootVc.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName: JFStyle.navigarionBarColor()], for: .selected)
+
+        /// 'NSFontAttributeName' has been renamed to 'NSAttributedStringKey.font'
+        rootVc.tabBarItem.setTitleTextAttributes([NSFontAttributeName:UIFont.systemFont(ofSize: 11.0)], for: .normal)
+        
+        //实例化导航控制器
+        let nav = JFBaseNacigationController(rootViewController: rootVc)
+        return nav
+    }
+    
+}
+
+//获取命名空间
+
+extension Bundle{
+    // 计算型属性类似于函数，没有参数，有返回值
+    var namespace: String{
+        return infoDictionary?["CFBundleName"] as? String ?? ""
+    }
+}
+
+
+
