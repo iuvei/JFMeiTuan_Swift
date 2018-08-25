78e400de9fa60f1b8b238fcb8ab82b7dce03ae65
tubie <594426995@qq.com>
2018-08-25 16:36:17 +0800

    添加附近的数据源

--- JFMeiTuan_Swift/JFMeiTuan_Swift/Classes/Order/View/JFNormalOrderCell.swift
+++ JFMeiTuan_Swift/JFMeiTuan_Swift/Classes/Order/View/JFNormalOrderCell.swift
@@ -23,6 +23,14 @@ class JFNormalOrderCell: UITableViewCell {
         didSet{
             //重写set方法
             normalImageView.image = UIImage.init(named:  (nearModel?.imageName)!)
+            
+            if (nearModel?.imageName?.hasPrefix("http"))! {
+                let url  = URL(string: (nearModel?.imageName)!)
+                normalImageView.kf.setImage(with: url)
+            }else{
+                normalImageView.image = UIImage.init(named:  (nearModel?.imageName)!)
+            }
+            
             moneyLabel.text = nearModel?.money
             addressLabel.text = nearModel?.adress
             distanceLabel.text = nearModel?.distance
